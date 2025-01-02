64ea52078aa019d288de9d857dd2ccb55de731f6
Socrates <scsantos492@gmail.com>
2025-01-02 18:18:29 -0300

    Adiciona rota para a URL base

--- vale_pedagio/urls.py
+++ vale_pedagio/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.http import HttpResponse
 from api.views import ValePedagioViewSet
 
 # Criação do roteador para as rotas automáticas
 router = DefaultRouter()
 router.register(r'vales', ValePedagioViewSet, basename='vale-pedagio')
 
+# View simples para a página inicial
+def home(request):
+    return HttpResponse("Bem-vindo ao Vale Pedágio API!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),  # Acesso ao admin
     path('api/', include(router.urls)),  # Inclui as rotas da API
+    path('', home),  # Define a rota para a URL base
 ]
